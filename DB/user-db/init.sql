282715676657601707e156ea856941791ff99ea2
Juan Esteban Escobar Restrepo <138255902+YugenDev@users.noreply.github.com>
2024-10-16 21:33:14 -0500

    Add SQL script for users DB

---
+++ DB/user-db/init.sql
@@ -0,0 +1,17 @@
+-- Crear la tabla 'users'
+CREATE TABLE IF NOT EXISTS users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    role VARCHAR(10) CHECK (role IN ('admin', 'user')) NOT NULL
+);
+
+-- Insertar datos de prueba
+INSERT INTO users (username, password, email, role)
+VALUES 
+    ('juan_admin', 'hashed_password_123', 'juan.admin@example.com', 'admin'),
+    ('maria_user', 'hashed_password_456', 'maria.user@example.com', 'user'),
+    ('carlos_user', 'hashed_password_789', 'carlos.user@example.com', 'user'),
+    ('ana_admin', 'hashed_password_abc', 'ana.admin@example.com', 'admin')
+ON CONFLICT DO NOTHING;
