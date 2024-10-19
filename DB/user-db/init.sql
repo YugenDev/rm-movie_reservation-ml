40f2907f7b14285ff4c21c90ecc979007ca7f842
yugoso8512 <yugoso8512@gmail.com>
2024-10-19 04:34:01 +0000

    Change password to password_hash

--- DB/user-db/init.sql
+++ DB/user-db/init.sql
@@ -2,13 +2,13 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     role VARCHAR(10) CHECK (role IN ('admin', 'user')) NOT NULL
 );
 
 -- Insertar datos de prueba
-INSERT INTO users (username, password, email, role)
+INSERT INTO users (username, password_hash, email, role)
 VALUES 
     ('juan_admin', 'hashed_password_123', 'juan.admin@example.com', 'admin'),
     ('maria_user', 'hashed_password_456', 'maria.user@example.com', 'user'),
