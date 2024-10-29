f238380186aa7ae0469b32de18e768279d12d651
yugoso8512 <yugoso8512@gmail.com>
2024-10-29 20:24:16 +0000

    Add PostgreSQL UUID specific impl

--- user-service/db/models/user.py
+++ user-service/db/models/user.py
@@ -1,6 +1,8 @@
-import enum
 from sqlalchemy import Column, String, Enum
-from config.session import Base
+from sqlalchemy.dialects.postgresql import UUID
+from db.session import Base
+import uuid
+import enum
 
 
 class UserRole(enum.Enum):
@@ -11,7 +13,7 @@ class UserRole(enum.Enum):
 class User(Base):
     __tablename__ = 'users'
 
-    user_id = Column(String, primary_key=True, index=True, unique=True, nullable=False, server_default='gen_random_uuid()')
+    user_id = Column(UUID(as_uuid=True), primary_key=True, index=True, unique=True, nullable=False, default=uuid.uuid4)
     username = Column(String, unique=True)
     password_hash = Column(String, nullable=False)
     email = Column(String, unique=True)
