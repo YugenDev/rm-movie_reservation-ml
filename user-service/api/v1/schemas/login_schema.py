0aad205976bd0e632f9dad757e98bbd94b327f0c
yugoso8512 <yugoso8512@gmail.com>
2024-11-05 20:09:01 +0000

    Making the linter happy! :)

--- user-service/api/v1/schemas/login_schema.py
+++ user-service/api/v1/schemas/login_schema.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel, EmailStr, constr
+from pydantic import BaseModel, EmailStr
 from enum import Enum
 
 
 class UserRole(str, Enum):
     admin = 'admin'
     user = 'user'
+
 
 class LoginSchema(BaseModel):
     email: EmailStr
