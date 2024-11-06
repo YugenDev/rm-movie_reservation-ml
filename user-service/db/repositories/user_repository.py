6599e102891e9c746e4cc947f45a44dabf2403b1
yugoso8512 <yugoso8512@gmail.com>
2024-11-06 21:17:06 +0000

    Add validations on update user method Only an user itself or an admin can change the user details, and only an admin can change roles.

--- user-service/db/repositories/user_repository.py
+++ user-service/db/repositories/user_repository.py
@@ -56,6 +56,10 @@ class UserRepository:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
         if user.username and db.query(User).filter(User.username == user.username, User.user_id != user_id).first():
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already taken")
+        if user.role is None:
+            user.role = existing_user.role
+        if existing_user.role == "user" and user.role is not None:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You do not have permission to change your role")
         try:
             for key, value in user.model_dump().items():
                 setattr(existing_user, key, value)
