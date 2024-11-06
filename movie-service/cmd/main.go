a7426d3e1aad9acd4e81cae1beba336cb36a6cf3
yugoso8512 <yugoso8512@gmail.com>
2024-11-06 20:06:08 +0000

    Add root/healthcheck endpoint! :)

--- movie-service/cmd/main.go
+++ movie-service/cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
 func main() {
-	
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Movie service is up! 🚀🫡")
+	})
+	e.Logger.Fatal(e.Start(":8081"))
 }
