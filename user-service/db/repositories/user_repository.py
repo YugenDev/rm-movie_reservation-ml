6d6cf7e3876b5faed430efa4701cc05c8c3b2d29
yugoso8512 <yugoso8512@gmail.com>
2024-11-06 00:49:28 +0000

    Add not found validations

--- user-service/db/repositories/user_repository.py
+++ user-service/db/repositories/user_repository.py
@@ -67,9 +67,9 @@ class UserRepository:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
     def delete_user(self, db: Session, user_id: str):
-        user = db.query(User).filter(User.user_id.as_string(binary=True) == user_id).first()
+        user = db.query(User).filter(User.user_id == user_id).first()
         if not user:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="aaaaaaaaaaaaaaaaaaaaaa")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
         try:
             db.delete(user)
             db.commit()
