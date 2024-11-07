4c8d0a0b5dce1d411d0fb5105a8242237ce3aca8
Juan Esteban Escobar Restrepo <138255902+YugenDev@users.noreply.github.com>
2024-11-06 22:50:05 -0500

    Add movie service Added methods to get all movies and add new ones with some basic validations.

--- movie-service/internal/services/movieService.go
+++ movie-service/internal/services/movieService.go
@@ -1,7 +1,37 @@
 package services
 
-import "github.com/YugenDev/rm-movie_service-ml/internal/models"
+import (
+	"github.com/YugenDev/rm-movie_service-ml/internal/models"
+	"github.com/YugenDev/rm-movie_service-ml/internal/repositories"
+	"github.com/labstack/echo"
+	"net/http"
+)
 
-func GetAllMovies() ([]models.Movie, error) {
-	return movieRepo.GetAllMovies()
+type MovieService struct {
+	Repo *repositories.MovieRepository
+}
+
+func NewMovieService(repo *repositories.MovieRepository) *MovieService {
+	return &MovieService{Repo: repo}
+}
+
+func (service *MovieService) GetAllMovies() ([]models.Movie, error) {
+	return service.Repo.GetAllMovies()
+}
+
+func (service *MovieService) CreateMovie(movie models.Movie) (models.Movie, error) {
+	movies, _ := service.Repo.GetAllMovies()
+	for _, m := range movies {
+		if m.Title == movie.Title && m.Description == movie.Description {
+			return models.Movie{}, echo.NewHTTPError(http.StatusBadRequest, "Movie already exists")
+		}
+	}
+	if movie.Title == "" {
+		return models.Movie{}, echo.NewHTTPError(http.StatusBadRequest, "title is required")
+	}
+	if movie.Description == "" {
+		return models.Movie{}, echo.NewHTTPError(http.StatusBadRequest, "description is required")
+	}
+
+	return service.Repo.CreateMovie(movie)
 }
