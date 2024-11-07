a9e84ee82b3b6fc4cd2a40a9c3ab0514782990ac
Juan Esteban Escobar Restrepo <138255902+YugenDev@users.noreply.github.com>
2024-11-06 22:44:57 -0500

    Add initialization of movie routes

--- movie-service/cmd/main.go
+++ movie-service/cmd/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/YugenDev/rm-movie_service-ml/internal/config"
+	"github.com/YugenDev/rm-movie_service-ml/internal/handlers"
+	"github.com/YugenDev/rm-movie_service-ml/internal/repositories"
+	"github.com/YugenDev/rm-movie_service-ml/internal/routes"
+	"github.com/YugenDev/rm-movie_service-ml/internal/services"
 	"github.com/labstack/echo"
+	"log"
+	"net/http"
 )
 
 func main() {
 
-	e := echo.New()
+	db, err := config.InitDB()
+	if err != nil {
+		log.Fatal("Error initializing database: ", err)
+	}
+	defer config.CloseDB(db)
 
+	movieRepo := repositories.NewMovieRepository(db)
+	movieService := services.NewMovieService(movieRepo)
+	movieHandler := handlers.NewMovieHandler(movieService)
+
+	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Movie service is up! 🐹🎥🚀")
 	})
+
+	routes.RegisterMovieRoutes(e, movieHandler)
+
 	e.Logger.Fatal(e.Start(":8081"))
 }
