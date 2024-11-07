7b0e72482f5e3c1f685d0204b3c393b654c8aeaa
Juan Esteban Escobar Restrepo <138255902+YugenDev@users.noreply.github.com>
2024-11-06 22:47:53 -0500

    Add Movie model using Gorm

--- movie-service/internal/models/movie.go
+++ movie-service/internal/models/movie.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Movie struct {
+	MovieId        string         `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"movie_id"`
+	Title          string         `gorm:"type:varchar(255);not null" json:"title"`
+	Description    string         `gorm:"type:text;not null" json:"description"`
+	PosterImageUrl string         `gorm:"type:varchar(255)" json:"poster_image_url"`
+	Genre          string         `gorm:"type:varchar(100)" json:"genre"`
+	CreatedAt      time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt      time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP" json:"updated_at"`
+	DeletedAt      gorm.DeletedAt `gorm:"index" json:"-"`
+}
