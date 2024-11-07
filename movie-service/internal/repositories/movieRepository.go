5d0e23e7d5c1aee8098d0f3863456872a4551f15
Juan Esteban Escobar Restrepo <138255902+YugenDev@users.noreply.github.com>
2024-11-06 22:48:39 -0500

    Add movie repo Added methods to get all movies and add new ones.

--- movie-service/internal/repositories/movieRepository.go
+++ movie-service/internal/repositories/movieRepository.go
@@ -1,1 +1,30 @@
 package repositories
+
+import (
+	"github.com/YugenDev/rm-movie_service-ml/internal/models"
+	"gorm.io/gorm"
+)
+
+type MovieRepository struct {
+	DB *gorm.DB
+}
+
+func NewMovieRepository(db *gorm.DB) *MovieRepository {
+	return &MovieRepository{DB: db}
+}
+
+func (repo *MovieRepository) GetAllMovies() ([]models.Movie, error) {
+	var movies []models.Movie
+	if err := repo.DB.Find(&movies).Error; err != nil {
+		return nil, err
+	}
+	return movies, nil
+}
+
+func (repo *MovieRepository) CreateMovie(movie models.Movie) (models.Movie, error) {
+	result := repo.DB.Create(&movie)
+	if result.Error != nil {
+		return models.Movie{}, result.Error
+	}
+	return movie, nil
+}
