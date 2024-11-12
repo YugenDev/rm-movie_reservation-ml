cd7d78b11a5044d9bb67c6e25fc065b224493726
yugoso8512 <yugoso8512@gmail.com>
2024-11-12 15:55:01 +0000

    Fix seat number type int -> String

--- showtime-service/src/main/java/com/yugendev/showtime_service/repository/SeatRepository.java
+++ showtime-service/src/main/java/com/yugendev/showtime_service/repository/SeatRepository.java
@@ -2,11 +2,16 @@ package com.yugendev.showtime_service.repository;
 
 import com.yugendev.showtime_service.model.Seat;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 public interface SeatRepository extends ReactiveCrudRepository<Seat, UUID> {
 
-    Mono<Seat> findByShowtimeIdAndSeatNumber(UUID showtimeId, int seatNumber);
+    Mono<Seat> findByShowtimeIdAndSeatNumber(UUID showtimeId, String seatNumber);
+
+    Flux<Seat> findAllByShowtimeId(UUID showtimeId);
+
 }
--- showtime-service/src/main/java/com/yugendev/showtime_service/service/SeatService.java
+++ showtime-service/src/main/java/com/yugendev/showtime_service/service/SeatService.java
@@ -4,6 +4,8 @@ import com.yugendev.showtime_service.model.Seat;
 import com.yugendev.showtime_service.repository.SeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -18,11 +20,17 @@ public class SeatService {
         this.seatRepository = seatRepository;
     }
 
-    public Mono<Seat> getSeatByShowtimeIdAndSeatNumber(UUID showtimeId, int seatNumber) {
-        return seatRepository.findByShowtimeIdAndSeatNumber(showtimeId, seatNumber);
-    }
-
     public Mono<Seat> createSeat(Seat seat) {
         return seatRepository.save(seat);
     }
+
+    public Flux<Seat> getSeatsByShowtimeId(UUID showtimeId) {
+        return seatRepository.findAllByShowtimeId(showtimeId);
+    }
+
+    public Mono<Seat> getSeatByShowtimeIdAndSeatNumber(UUID showtimeId, String seatNumber) {
+        return seatRepository.findByShowtimeIdAndSeatNumber(showtimeId, seatNumber);
+    }
+
+
 }
