c2508a7d3535e1d7f86be7054a7155aa5ce17153
Juan Esteban Escobar Restrepo <138255902+YugenDev@users.noreply.github.com>
2024-11-14 01:08:03 +0000

    Add rabbitMQ connections

--- showtime-service/src/main/java/com/yugendev/showtime_service/config/RabbitMQConfig.java
+++ showtime-service/src/main/java/com/yugendev/showtime_service/config/RabbitMQConfig.java
@@ -1,16 +1,18 @@
 package com.yugendev.showtime_service.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
     @Bean
-    public Queue<String> showtimeQueue() {
-        return new Queue<>("showtimeQueue", true);
+    public Queue showtimeQueue() {
+        return new Queue("showtimeQueue", true);
     }
     
     @Bean
@@ -18,4 +20,18 @@ public class RabbitMQConfig {
         return new Queue("reservationQueue", true);
     }
 
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange("movieExchange");
+    }
+
+    @Bean
+    public Binding showtimeBinding(Queue showtimeQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(showtimeQueue).to(exchange).with("showtime");
+    }
+
+    @Bean
+    public Binding reservationBinding(Queue reservationQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(reservationQueue).to(exchange).with("reservation");
+    }
 }
