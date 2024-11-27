ab932ba934ec8d8c6612c400d305793662200fa3
Juan Esteban Escobar Restrepo <138255902+YugenDev@users.noreply.github.com>
2024-11-26 23:20:48 -0500

    Add Repository annotations

--- showtime-service/src/main/java/com/yugendev/showtime_service/repository/SeatRepository.java
+++ showtime-service/src/main/java/com/yugendev/showtime_service/repository/SeatRepository.java
@@ -3,12 +3,14 @@ package com.yugendev.showtime_service.repository;
 import com.yugendev.showtime_service.model.Seat;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface SeatRepository extends ReactiveCrudRepository<Seat, UUID> {
 
     Mono<Seat> findByShowtimeIdAndSeatNumber(UUID showtimeId, String seatNumber);
--- showtime-service/src/main/java/com/yugendev/showtime_service/repository/ShowtimeRepository.java
+++ showtime-service/src/main/java/com/yugendev/showtime_service/repository/ShowtimeRepository.java
@@ -2,10 +2,12 @@ package com.yugendev.showtime_service.repository;
 
 import com.yugendev.showtime_service.model.Showtime;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+@Repository
 public interface ShowtimeRepository extends ReactiveCrudRepository<Showtime, UUID> {
 
     Mono<Showtime> findByShowtimeId(UUID showtimeId);
