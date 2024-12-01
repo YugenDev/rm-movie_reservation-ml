b781b68cc182796b7fd5880746224e1c45ecb219
Juan Esteban Escobar Restrepo <138255902+YugenDev@users.noreply.github.com>
2024-12-01 15:05:28 -0500

    Delete listener

--- showtime-service/src/main/java/com/yugendev/showtime_service/service/broker_service/ShowtimeListener.java
+++ showtime-service/src/main/java/com/yugendev/showtime_service/service/broker_service/ShowtimeListener.java
@@ -1,10 +1,8 @@
 package com.yugendev.showtime_service.service.broker_service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 import com.yugendev.showtime_service.service.ShowtimeService;
-import com.yugendev.showtime_service.service.broker_service.message.ReservationMessage;
 
 @Service
 public class ShowtimeListener {
@@ -13,12 +11,6 @@ public class ShowtimeListener {
 
     public ShowtimeListener(ShowtimeService showtimeService) {
         this.showtimeService = showtimeService;
-    }
-
-    @RabbitListener(queues = "reservationQueue")
-    public void receiveMessage(ReservationMessage message) {
-        showtimeService.reserveSeats(message.getShowtimeId(), message.getSeatNumbers())
-        .subscribe();
     }
 
 
